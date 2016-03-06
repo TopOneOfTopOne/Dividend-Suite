7dfcd3e3402e207f00cfdaa3be54a1076f11fe35
noobling <21711933@student.uwa.edu.au>
2016-03-06 19:57:30 +0800

    removed uneeded code from stock info scraper

--- scraping/stock_info_scraper.rb
+++ scraping/stock_info_scraper.rb
@@ -7,7 +7,6 @@ module StockInfo
 	STOCK_QUOTES_URL =  'http://www.morningstar.com.au/Stocks/NewsAndQuotes/'
 	inFile = YAML.load_file(File.expand_path("../data/#{stocks_to_scrape_file}",File.dirname(__FILE__)))
 	outFile = File.open(File.expand_path("../data/stock_info.yml", File.dirname(__FILE__)),'w')
-	@stocks_info = []
 
 	def self.get_info(code)
 		puts "Attempting to get #{code}"
@@ -25,37 +24,9 @@ module StockInfo
 		puts "\t Success!"
 	end
 
-	def self.get_single_stock_info(code)
-	end
-
 	def self.get_info_from_array(array = [])
 		array.map {|code| get_info(code)}
 	end
-
-	# def self.add_to_stocks_info_array(code)
-	# 	last_price, open_price, day_range, year_range, market_cap, prev_close, today_volume, month_avg_volume, outstanding_shares, sector, industry = get_info(code)
-	# 	@stocks_info <<  
-	# 	{
-	# 		last_price: last_price, open_price: open_price, day_range: day_range, year_range: year_range, market_cap: market_cap, \
-	# 		prev_close: prev_close, today_volume: today_volume, month_avg_volume: month_avg_volume, \
-	# 		outstanding_shares: outstanding_shares, sector: sector, industry: industry
-	# 	}
-	# 	sleep(2) # If you bombard MorningStar they seem to delay your requests
-	# end
-
-	# def self.get_info_from_file
-	# @stocks_info = []
-	# inFile.each do |stock|
-	# 	last_price, open_price, day_range, year_range, market_cap, prev_close, today_volume, month_avg_volume, outstanding_shares, sector, industry = get_info(stock[:code])
-	# 	@stocks_info <<  
-	# 	{
-	# 		open_price: open_price, day_range: day_range, year_range: year_range, market_cap: market_cap, \
-	# 		prev_close: prev_close, today_volume: today_volume, month_avg_volume: month_avg_volume, \
-	# 		outstanding_shares: outstanding_shares, sector: sector, industry: industry
-	# 	}
-	# 	sleep(1)
-	# end
-	# outFile.puts stock_info
 end
 
 # p StockInfo.get_info_from_array(['CBA', 'RIO', 'CIN'])
